7e2458fc429f79122aca6cc183fd150f92c916a0
Andrey Germanov <andrey@it-port.ru>
2018-02-05 12:31:01 +0300

    Initial commit

---
+++ app/src/main/java/ru/itport/andrey/myplaces/PlacesList.java
@@ -0,0 +1,13 @@
+package ru.itport.andrey.myplaces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlacesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places_list);
+    }
+}
